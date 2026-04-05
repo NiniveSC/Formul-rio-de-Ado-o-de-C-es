e39cb51d75f3ad7b13fe08866d776f9469b31232
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-04-05 17:04:01 -0300

    add - terminei os campos obrigatorios no java script

--- script.js
+++ script.js
@@ -18,9 +18,75 @@ form.addEventListener("submit", function (event) {
     const motivo = document.getElementById("motivo").value.trim();
     const termo = document.getElementById("termo").checked;
 
+    if (nome.length < 3) {
+        alert("O Nome deve ter no mínimo 3 caracteres.");
+        return;
+    }
+
+    if (!email.includes("@")) {
+        alert("Email inválido.");
+        return;
+    }
+
+    if (telefone.length < 8) {
+        alert("Telefone inválido.");
+        return;
+    }
+
+    if (!cpf) {
+        alert("CPF obrigatório.");
+        return;
+    }
+
+    if (cpfsCadastrados.includes(cpf)) {
+        alert("CPF já cadastrado.");
+        return;
+    }
+
+    if (idade < 18) {
+        alert("Você precisa ter 18 anos ou mais para adotar");
+        return;
+    }
+
+    if (!cidade) {
+        alert("Cidade obrigatória.");
+        return;
+    }
+
+    if (!moradia) {
+        alert("Selecione o tipo de moradia.");
+        return;
+    }
+
+    if (!quintal) {
+        alert("Informe se possui quintal.")
+        return;
+    }
+
+    if (!pet) {
+        alert("Informe se já teve pet.");
+        return;
+    }
+
+    if (isNaN(horas)) {
+        alert("Informe um número válido de horas")
+        return;
+    }
+
+    if (motivo.length < 10) {
+        alert("O motivo deve ter pelo menos 10 caracteres.");
+        return;
+    }
+
+    if (!termo) {
+        alert("Você deve aceitar os termos.");
+        return;
+    }
+
+
+
+
+
     
-
-
-
 
 });
